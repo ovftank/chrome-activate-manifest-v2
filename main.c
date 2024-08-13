530e18802efad13b09ca97385db2ab8685210d71
Tuấn Anh <tripleseven190504@gmail.com>
2024-08-13 14:33:56 +0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main() {
+    system("reg add \"HKLM\\Software\\Policies\\Google\\Chrome\" /v ExtensionManifestV2Availability /t REG_DWORD /d 2 /f");
+    ShellExecute(0, "open", "https://github.com/tripleseven190504", 0, 0, SW_SHOWNORMAL);
+    return 0;
+}
